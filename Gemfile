dc9abed9b12ce3b12e6382d4bc9c135537eb2df0
Arslan Rao <Arslanrao94@gmail.com>
2019-12-30 23:12:43 +0500

    Added sms notifications

--- Gemfile
+++ Gemfile
@@ -57,4 +57,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'exception_notification'
 gem 'font-awesome-rails'
+gem 'twilio-ruby', '~> 5.31.1'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+require 'twilio-ruby'
 class HomeController < ApplicationController
   def index
   end
@@ -11,15 +12,22 @@ class HomeController < ApplicationController
 
       UserMailer.send_booking(@booking).deliver_later
 
+      send_sms(@booking)
+
       redirect_to booking_path
     else
       render :booking
     end
   end
 
-
   private
   def booking_params
     params.require(:appointment).permit(:name, :phone, :service, :address)
+  end
+
+  def send_sms booking
+    @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_ACCOUNT_SID'])
+    body = 'Booking Detail: -- Name: ' + booking.name + ' -- Phone: ' + booking.phone + ' -- Service: ' + booking.service + ' -- Address: ' + booking.address + ' --'
+    message = @client.messages.create(body: body,from: '+19729925765',to: '+923347567120')
   end
 end
