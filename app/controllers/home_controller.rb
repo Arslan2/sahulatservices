0c6e7814b135f3350d7375c4c4d66fa38d6d1d36
Arslan Rao <Arslanrao94@gmail.com>
2019-12-30 23:16:48 +0500

    Added sms notifications

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,6 @@ class HomeController < ApplicationController
       UserMailer.send_booking(@booking).deliver_later
 
       send_sms(@booking)
-
       redirect_to booking_path
     else
       render :booking
@@ -26,7 +25,12 @@ class HomeController < ApplicationController
   end
 
   def send_sms booking
-    @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_ACCOUNT_SID'])
+    account_sid = 'AC1920de72a2f3508949c2c7355d8a278a'
+    auth_token = '228da7fae7b513217510dc063fe539a9'
+
+    #@client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_ACCOUNT_SID'])
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
     body = 'Booking Detail: -- Name: ' + booking.name + ' -- Phone: ' + booking.phone + ' -- Service: ' + booking.service + ' -- Address: ' + booking.address + ' --'
     message = @client.messages.create(body: body,from: '+19729925765',to: '+923347567120')
   end
